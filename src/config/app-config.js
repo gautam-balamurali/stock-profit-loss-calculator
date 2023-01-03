63d714e639646b22266ac6c11513fb20e3c08bd6
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-01-03 23:53:21 +0530

    fix: handled input validations

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 
 const DEFAULT_CONSTANTS = {
-  INVALID_INPUT_MESSAGE: "Input should be a valid number.",
+  INVALID_INPUT_MESSAGE:
+    "Input should be a valid number. Please use some common sense or else watch pogo.🙂",
   APP_DESCRIPTION: "Enter your stock details to calculate your profit or loss.",
   DEFAULT_OUTPUT: "NO PAIN NO GAIN AND NO GAIN NO PAIN.🥱",
 };
